142a2fc8be5710f3449c2fae448d537c52df7ed0
fglukhov <f.glukhov@gmail.com>
2014-12-21 12:47:58 +0300

    Ver. 2.0
    New pages

--- js/template.js
+++ js/template.js
@@ -1,5 +1,110 @@
 ﻿$(document).ready(function() {
 
+	$(".btn-want-to").click(function() {
+		if ($("html").scrollTop() > 0 || $("body").scrollTop() > 0) {
+			if ($("#searchForm").css("display") == "block") {
+				$(".btn-search").click();
+			}
+			if ($("#userMenu").css("display") == "block") {
+				$(".user-menu-toggler").click();
+			}
+			$("html,body").animate({
+				scrollTop:0
+			},500,function() {
+				handAnimation();
+			});
+			
+		} else {
+			var delay = 0;
+			if ($("#searchForm").css("display") == "block") {
+				$(".btn-search").click();
+				delay = 500;
+			}
+			if ($("#userMenu").css("display") == "block") {
+				$(".user-menu-toggler").click();
+				delay = 500;
+			}
+			
+			if (handTimeout) {
+				clearTimeout(handTimeout)
+			}
+			
+			var handTimeout = setTimeout(function() {
+				handAnimation();
+			},delay)
+			
+		}
+	})
+
+	
+	// Удаление уведомлений
+	
+	$(".notifications-item .close").click(function() {
+		var notification = $(this).parents(".notifications-item");
+		notification.slideUp(150,function() {
+			// удаление сообщения
+			notification.remove();
+		})
+	});
+	
+	$(".notifications-remove").click(function() {
+		var trigger = $(this);
+		var notification = $(".notifications-item");
+		notification.slideUp(150,function() {
+			// удаление сообщения
+			notification.remove();
+		});
+		trigger.slideUp(150,function() {
+			// удаление кнопки
+			notification.remove();
+		});
+	})
+
+	// Переключение баннеров
+	
+	$(".banners-nav .nav-item").click(function() {
+		if (!$(this).hasClass("act")) {
+			
+			var banners = $(this).parents(".banners").find(".banners-item");
+			
+			banners.hide().removeClass("act");
+			
+			banners.eq($(this).prevAll(".nav-item").length).show().addClass("act");
+			
+			$(this).siblings(".nav-item").removeClass("act");
+			$(this).addClass("act");
+		
+		}
+	})
+	
+	// Переключение баннеров END
+
+	// События пользователя
+	
+	if ($("#userEventsCarousel").length) {
+		$("#userEventsCarousel").carousel({
+			interval: false
+		})
+		
+		var uEvRange = $("#userEventsCarousel .range");
+		var uEvTotal = $("#userEventsCarousel .total");
+		
+		uEvFrom = $("#userEventsCarousel .item.active").prevAll(".item").find(".user-events-item").length + 1;
+		uEvTo = uEvFrom + $("#userEventsCarousel .item.active").find(".user-events-item").length - 1
+		uEvRange.html(uEvFrom + "&ndash;" + uEvTo)
+		uEvTotal.html($("#userEventsCarousel .user-events-item").length)
+		
+		$('#userEventsCarousel').on('slid.bs.carousel', function () {
+			uEvFrom = $("#userEventsCarousel .item.active").prevAll(".item").find(".user-events-item").length + 1;
+			uEvTo = uEvFrom + $("#userEventsCarousel .item.active").find(".user-events-item").length - 1
+			uEvRange.html(uEvFrom + "&ndash;" + uEvTo)
+			uEvTotal.html($("#userEventsCarousel .user-events-item").length)
+		})
+	}
+	
+	// События пользователя END
+	
+
 	$('.main-carousel').carousel()
 
 	$(".user-menu-toggler").click(function() {
@@ -83,3 +188,27 @@
 	});
 	
 });
+
+function handAnimation() {
+	var actIndex = 0;
+	if (animInt) {
+		clearInterval(animInt)
+	}
+	$(".hand-anim").show();
+	var animInt = setInterval(function() {
+		if (actIndex < 8){
+			$(".hand-anim img").hide();
+			$(".hand-anim img").eq(actIndex).show();
+			
+			if (actIndex == 5) {
+				$(".notifications-count").show();
+			}
+			
+			actIndex++;
+		} else {
+			$(".hand-anim img").hide();
+			$(".hand-anim").hide();
+			clearInterval(animInt)
+		}
+	},70)
+}
